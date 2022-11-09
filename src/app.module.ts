cdebf00a1784b5e33d0f23b2d9f65dfec099645c
Fabricio ALEGRIA <80031577+ahfabricio@users.noreply.github.com>
2022-11-09 15:00:08 +0100

    feat(17): POST /decisions is able to receive wordperfect files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { HttpModule } from '@nestjs/axios'
+import { DecisionsModule } from './infrastructure/controllers/decisions/decisions.module'
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, DecisionsModule],
   controllers: [],
   providers: []
 })
