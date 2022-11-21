2f347c98d3d5dd4c81b985ba88d7a3e6b4fddc09
Armen Ozcelik <armen.ozcelik@free.fr>
2022-11-21 17:45:48 +0100

    tech: add API Documentation with Swagger

---
+++ src/infrastructure/controllers/decisions/dto/collect.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { MetadonneesDto } from './metadonnees.dto'
+
+export class CollectDto {
+  @ApiProperty({
+    type: 'string',
+    format: 'binary',
+    description: 'Décision intègre au format wordperfect.'
+  })
+  decisionIntegre: any
+
+  @ApiProperty({ description: 'Metadonnées associées à la décision intègre.' })
+  metadonnees: MetadonneesDto
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import * as basicAuth from 'express-basic-auth'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  // Add login/password to access to API Documentation
+  const basicAuthOptions: basicAuth.IUsersOptions = {
+    challenge: true,
+    users: {}
+  }
+  basicAuthOptions.users[process.env.DOC_LOGIN] = process.env.DOC_PASSWORD
+  app.use(['/doc', '/doc-json'], basicAuth(basicAuthOptions))
+
+  // Add API Documentation with Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API JuriTJ')
+    .setDescription("Documentation de l'API JuriTJ. ")
+    .setVersion('1.0')
+    .addTag('Collect')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('doc', app, document)
+
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
