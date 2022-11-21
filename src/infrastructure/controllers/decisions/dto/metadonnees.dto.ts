1bab8ff270286c0a06ce7e40ff50b3134ec089c8
Armen Ozcelik <armen.ozcelik@octo.com>
2022-11-21 17:45:48 +0100

    tech: add API Documentation with Swagger

--- src/infrastructure/controllers/decisions/dto/metadonnees.dto.ts
+++ src/infrastructure/controllers/decisions/dto/metadonnees.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsString } from 'class-validator'
 
 export class MetadonneesDto {
+  @ApiProperty({
+    description: 'Nom de la juridiction émetrice de la décision.',
+    type: String
+  })
   @IsString()
   juridictionName: string
 }
