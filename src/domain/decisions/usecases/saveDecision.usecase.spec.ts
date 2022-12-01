edb9bf6be3f01ed754be36cdbfdd079589944b8f
Fabricio ALEGRIA <f.alegria.herrera@octo.com>
2022-12-01 10:58:26 +0100

    feat(21): POST /decisions uses S3 to save decisions with metadata

---
+++ src/domain/decisions/usecases/saveDecision.usecase.spec.ts
@@ -0,0 +1,13 @@
+import { saveDecisionUsecase } from './saveDecision.usecase'
+
+describe('saveDecision', () => {
+  const usecase = new saveDecisionUsecase()
+
+  it('returns decision object when decision is saved successfully', () => {
+    // GIVEN
+    const decision = {}
+    // WHEN
+
+    // THEN
+  })
+})
