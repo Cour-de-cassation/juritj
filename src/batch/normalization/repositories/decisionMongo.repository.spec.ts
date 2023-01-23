621c71e677a1d6b9aa79bdfc8d2e41ad9b3965a4
Armen Ozcelik <armen.ozcelik@octo.com>
2023-01-23 23:31:41 +0100

    tech(191): fix tests

--- src/batch/normalization/repositories/decisionMongo.repository.spec.ts
+++ src/batch/normalization/repositories/decisionMongo.repository.spec.ts
@@ -1,6 +1,7 @@
 import { ServiceUnavailableException } from '@nestjs/common'
 import { MockUtils } from '../../../shared/infrastructure/utils/mock.utils'
 import { DecisionMongoRepository } from './decisionMongo.repository'
+import mongoose from 'mongoose'
 
 jest.mock('../normalization', () => ({
   logger: {
@@ -13,30 +14,37 @@ describe('saveDecision', () => {
   const mockMetadonnees = new MockUtils().metadonneesDtoMock
   const mongoRepository = new DecisionMongoRepository()
 
-  // TODO : mocker le saveDecision fait qu'on n'est pas assez précis, on mock la totalité de l'implem de la fonction qu'on teste
   it('throws an error when connection to mongo failed', async () => {
     // GIVEN
-    jest.spyOn(mongoRepository, 'saveDecision').mockImplementationOnce(() => {
-      throw new ServiceUnavailableException('Error from Mongo')
+    jest.spyOn(mongoose, 'connect').mockImplementation(() => {
+      throw new ServiceUnavailableException('Error from database')
     })
 
     expect(
       // WHEN
-      () => mongoRepository.saveDecision(mockMetadonnees)
+      mongoRepository.saveDecision(mockMetadonnees)
     )
       // THEN
-      .toThrowError()
+      .rejects.toEqual(new ServiceUnavailableException('Error from database'))
   })
 
-  // TODO : mocker le saveDecision fait qu'on n'est pas assez précis, on mock la totalité de l'implem de la fonction qu'on teste
   it('calls the function to insert data to the collection', async () => {
     // GIVEN
-    jest.spyOn(mongoRepository, 'insertMetadonnees')
-    const saveDecisionFn = jest.spyOn(mongoRepository, 'saveDecision')
-    saveDecisionFn.mockImplementationOnce(jest.fn())
+    jest.spyOn(mongoose, 'connect').mockImplementation(() => {
+      return {
+        model: jest.fn()
+      } as any
+    })
+    const insertDataMock = jest
+      .spyOn(mongoRepository, 'insertMetadonnees')
+      .mockImplementation(async () => {
+        return mockMetadonnees
+      })
+
     // WHEN
-    mongoRepository.saveDecision(mockMetadonnees)
+    await mongoRepository.saveDecision(mockMetadonnees)
+
     // THEN
-    expect(saveDecisionFn).toHaveBeenCalled()
+    expect(insertDataMock).toHaveBeenCalled()
   })
 })
