718b019907303951292705d051d93a0e02cfe7c4
Fabricio ALEGRIA <f.alegria.herrera@octo.com>
2023-01-31 16:58:51 +0100

    tech(39): rebase master

--- src/batch/normalization/index.ts
+++ src/batch/normalization/index.ts
@@ -4,19 +4,18 @@ import { CustomLogger } from '../../shared/infrastructure/utils/customLogger.uti
 import { MockUtils } from '../../shared/infrastructure/utils/mock.utils'
 import { normalizationJob } from './normalization'
 
-const decisionName = '2022-12-21T14:59:36.878ZDecision_TJ_exemple2.wpd'
 const decisionContent = new MockUtils().decisionContent
 const EXIT_ERROR_CODE = 1
 
 export const normalizationContext = new Context()
 export const logger = new CustomLogger(normalizationContext)
-
+// final cron : '0 * * * *'
 function startJob() {
   const cron = new CronJob({
     cronTime: '* * * * *',
     onTick() {
       try {
-        normalizationJob(decisionName, decisionContent)
+        normalizationJob(decisionContent)
       } catch (error) {
         logger.error(error)
         logger.log('leaving normalization')
--- src/batch/normalization/normalization.ts
+++ src/batch/normalization/normalization.ts
@@ -6,7 +6,7 @@ import { removeUnnecessaryCharacters } from './services/removeUnnecessaryCharact
 import { ConvertedDecisionWithMetadonneesDto } from '../../shared/infrastructure/dto/convertedDecisionWithMetadonnees.dto'
 import { normalizationContext, logger } from './index'
 import { fetchDecisionListFromS3 } from './services/fetchDecisionListFromS3'
-import { DecisionS3Repository } from 'src/shared/infrastructure/repositories/decisionS3.repository'
+import { DecisionS3Repository } from '../../shared/infrastructure/repositories/decisionS3.repository'
 import { DecisionMongoRepository } from './repositories/decisionMongo.repository'
 
 const decisionMongoRepository = new DecisionMongoRepository()
@@ -18,50 +18,50 @@ export async function normalizationJob(
 ): Promise<ConvertedDecisionWithMetadonneesDto[]> {
   const listConvertedDecision: ConvertedDecisionWithMetadonneesDto[] = []
 
-    normalizationContext.start()
-    normalizationContext.setCorrelationId(uuidv4())
-    const decisionList = await fetchDecisionListFromS3()
-    if (decisionList.length > 0) {
-      for (const decisionName of decisionList) {
-        const decision = await s3Repository.getDecisionByFilename(decisionName)
-        const metadonnees = decision.metadonnees
+  normalizationContext.start()
+  normalizationContext.setCorrelationId(uuidv4())
+  const decisionList = await fetchDecisionListFromS3()
+  if (decisionList.length > 0) {
+    for (const decisionName of decisionList) {
+      const decision = await s3Repository.getDecisionByFilename(decisionName)
+      const metadonnees = decision.metadonnees
 
-        logger.log('[NORMALIZATION JOB] Normalization job starting for decision ' + decisionName)
+      logger.log('[NORMALIZATION JOB] Normalization job starting for decision ' + decisionName)
 
-        const idDecision = generateUniqueId(metadonnees)
-        logger.log('[NORMALIZATION JOB] Decision ID generated', idDecision)
+      const idDecision = generateUniqueId(metadonnees)
+      logger.log('[NORMALIZATION JOB] Decision ID generated', idDecision)
 
-        const cleanedDecision = removeUnnecessaryCharacters(decisionContent)
-        logger.log('[NORMALIZATION JOB] Unnecessary characters removed from decision.', idDecision)
+      const cleanedDecision = removeUnnecessaryCharacters(decisionContent)
+      logger.log('[NORMALIZATION JOB] Unnecessary characters removed from decision.', idDecision)
 
-        const convertedDecision = normalizeDatesToIso8601(cleanedDecision)
-        logger.log('[NORMALIZATION JOB] Decision dates converted to ISO8601.', idDecision)
+      const convertedDecision = normalizeDatesToIso8601(cleanedDecision)
+      logger.log('[NORMALIZATION JOB] Decision dates converted to ISO8601.', idDecision)
 
-        const transformedMetadonnees: Metadonnees = metadonnees
-        transformedMetadonnees.idDecision = idDecision
-        await decisionMongoRepository.saveDecision(transformedMetadonnees)
-        logger.log('[NORMALIZATION JOB] Metadonnees saved in database.', idDecision)
+      const transformedMetadonnees: Metadonnees = metadonnees
+      transformedMetadonnees.idDecision = idDecision
+      await decisionMongoRepository.saveDecision(transformedMetadonnees)
+      logger.log('[NORMALIZATION JOB] Metadonnees saved in database.', idDecision)
 
-        decision.metadonnees = transformedMetadonnees
-        await s3Repository.saveDecisionNormalisee(JSON.stringify(decision), decisionName)
-        logger.log('[NORMALIZATION JOB] Metadonnees saved in normalized bucket.', idDecision)
+      decision.metadonnees = transformedMetadonnees
+      await s3Repository.saveDecisionNormalisee(JSON.stringify(decision), decisionName)
+      logger.log('[NORMALIZATION JOB] Metadonnees saved in normalized bucket.', idDecision)
 
-        await s3Repository.deleteDecision(decisionName, bucketNameIntegre)
-        logger.log('[NORMALIZATION JOB] Decision deleted in raw bucket.', idDecision)
+      await s3Repository.deleteDecision(decisionName, bucketNameIntegre)
+      logger.log('[NORMALIZATION JOB] Decision deleted in raw bucket.', idDecision)
 
-        logger.log(
-          '[NORMALIZATION JOB] End of normalization job for decision ' + decisionName,
-          idDecision
-        )
-        listConvertedDecision.push({
-          metadonnees: transformedMetadonnees,
-          decisionNormalisee: convertedDecision
-        })
-      }
-
-      return listConvertedDecision
-    } else {
-      logger.log('No decisions found to normalize... Exiting now')
-      return []
+      logger.log(
+        '[NORMALIZATION JOB] End of normalization job for decision ' + decisionName,
+        idDecision
+      )
+      listConvertedDecision.push({
+        metadonnees: transformedMetadonnees,
+        decisionNormalisee: convertedDecision
+      })
     }
+
+    return listConvertedDecision
+  } else {
+    logger.log('No decisions found to normalize... Exiting now')
+    return []
+  }
 }
