776c67524f92006488217192d9ebaf0a5fb73773
Fabricio ALEGRIA <f.alegria.herrera@octo.com>
2023-02-01 15:20:07 +0100

    tech(39): put to variable the cron string to be more generic

--- src/batch/normalization/index.ts
+++ src/batch/normalization/index.ts
@@ -6,13 +6,13 @@ import { normalizationJob } from './normalization'
 
 const decisionContent = new MockUtils().decisionContent
 const EXIT_ERROR_CODE = 1
+const CRON_EVERY_HOUR = '0 * * * *'
 
 export const normalizationContext = new Context()
 export const logger = new CustomLogger(normalizationContext)
-// final cron : '0 * * * *'
 function startJob() {
   const cron = new CronJob({
-    cronTime: '* * * * *',
+    cronTime: CRON_EVERY_HOUR,
     onTick() {
       try {
         normalizationJob(decisionContent)
