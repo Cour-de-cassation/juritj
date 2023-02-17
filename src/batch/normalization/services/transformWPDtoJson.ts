40ada7fae4ca2ae83de50ae87531194592024976
maxg-octo <maxime.gatineau@octo.com>
2023-02-17 10:59:03 +0100

    tech(40): adds function to get a command path

--- src/batch/normalization/services/transformWPDtoJson.ts
+++ src/batch/normalization/services/transformWPDtoJson.ts
@@ -3,6 +3,28 @@ import { exec } from 'child_process'
 const execPromise = promisify(exec)
 
 export async function readWpd(filename: string) {
-  const { stdout } = await execPromise('/usr/local/bin/wpd2text ./' + filename)
+  console.log(await getCommandPath('wpd2text'))
+
+  // TO DO : voir si le fichier existe .wpd
+  const { stdout, stderr } = await execPromise('wpd2text ./' + filename)
+  // console.log('error');
+  // console.log(stderr);
+  // console.log('sortie');
+  // console.log(stdout);
+
   return stdout
 }
+
+export async function getCommandPath(commandName: string): Promise<string> {
+  // TO DO : condition que wpd2text existe et récupérer le path
+  return await execPromise('which ' + commandName)
+    .then((response) => {
+      return response.stdout.replace(/\n/g, '')
+    })
+    .catch((e) => {
+      console.log(e)
+      return null
+    })
+}
+
+readWpd('business_plan2.wpd')
