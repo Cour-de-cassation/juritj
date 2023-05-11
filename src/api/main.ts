39437d198bb36baa3f86a718558903b3256a2ab8
Armen Ozcelik <armen.ozcelik@octo.com>
2023-05-11 11:59:30 +0200

    feat(300): add auto-signed certificate for local and development environments tests

--- src/api/main.ts
+++ src/api/main.ts
@@ -20,7 +20,11 @@ async function bootstrap() {
     rejectUnauthorized: true,
     ca: [winciAuthorityCertificate]
   }
-  serverPrivateKeyPassphrase ? (httpsOptions.passphrase = serverPrivateKeyPassphrase) : null
+  httpsOptions.passphrase = serverPrivateKeyPassphrase ?? null
+
+  if (process.env.CURRENT_ENV === 'local' || process.env.CURRENT_ENV === 'dev') {
+    httpsOptions.ca.push(process.env.AUTO_SIGNED_CA_CERT)
+  }
 
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn'],
