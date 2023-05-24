900ed7eb6a65ab8b0d6cabfa5096fe645826c03f
Fabricio ALEGRIA <80031577+ahfabricio@users.noreply.github.com>
2023-05-24 15:55:10 +0200

    feat(367): add new value to valid mimetypes

---
+++ src/api/infrastructure/controllers/decisions/decisions.controller.spec.ts
@@ -0,0 +1,66 @@
+import { Readable } from 'stream'
+import { isWordperfectFileType } from './decisions.controller'
+
+describe('Decision Controller - Unit tests', () => {
+  describe('isWordperfectFileType', () => {
+    const file: Express.Multer.File = {
+      buffer: Buffer.from('text'),
+      destination: '',
+      encoding: '',
+      fieldname: '',
+      filename: '',
+      mimetype: '',
+      originalname: 'example.wpd',
+      path: '',
+      size: 0,
+      stream: new Readable()
+    }
+
+    it('returns false if the file does not have .wpd extension', () => {
+      // GIVEN
+      const wrongFile = { ...file, originalname: 'wrongFile.txt' }
+      // WHEN
+      const result = isWordperfectFileType(wrongFile)
+
+      // THEN
+      expect(result).toEqual(false)
+    })
+
+    it('returns false if the file does not have a valid wordperfect mimeType', () => {
+      // GIVEN
+      const wrongMimeTypeFile = {
+        ...file,
+        originalname: 'test.wpd',
+        mimetype: 'application/json'
+      }
+
+      // WHEN
+      const result = isWordperfectFileType(wrongMimeTypeFile)
+
+      // THEN
+      expect(result).toEqual(false)
+    })
+
+    it('return true if the file is a wpd file and has the good mimetype', () => {
+      // GIVEN
+      const validFile = {
+        ...file,
+        originalname: 'test.wpd',
+        mimetype: 'application/vnd.wordperfect'
+      }
+      const secondValidFile = {
+        ...file,
+        originalname: 'test.wpd',
+        mimetype: 'application/wordperfect'
+      }
+
+      // WHEN
+      const result = isWordperfectFileType(validFile)
+      const secondResult = isWordperfectFileType(secondValidFile)
+
+      // THEN
+      expect(result).toEqual(true)
+      expect(secondResult).toEqual(true)
+    })
+  })
+})
