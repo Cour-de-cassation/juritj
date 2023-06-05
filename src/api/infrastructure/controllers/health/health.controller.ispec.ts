68470f478a5267872d20621a41006dbc0b8a564a
Armen Ozcelik <armen.ozcelik@octo.com>
2023-06-05 17:44:08 +0200

    tech(369): add health endpoint to monitor the API and its dependencies

---
+++ src/api/infrastructure/controllers/health/health.controller.ispec.ts
@@ -0,0 +1,61 @@
+import * as request from 'supertest'
+import { INestApplication, HttpStatus } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3'
+import { mockClient, AwsClientStub } from 'aws-sdk-client-mock'
+import { AppModule } from '../../../app.module'
+import { Context } from '../../../..//shared/infrastructure/utils/context'
+import { RequestLoggerInterceptor } from '../../interceptors/request-logger.interceptor'
+
+describe('HealthController', () => {
+  let app: INestApplication
+  const mockS3: AwsClientStub<S3Client> = mockClient(S3Client)
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+
+    // Disable logs for Integration tests
+    app = moduleFixture.createNestApplication({ logger: false })
+    app.useGlobalInterceptors(new RequestLoggerInterceptor(new Context()))
+
+    await app.init()
+  })
+
+  beforeEach(() => {
+    mockS3.reset()
+  })
+
+  describe('GET /health', () => {
+    it('returns a 200 OK with bucket status UP when bucket is available', async () => {
+      // GIVEN
+      mockS3.on(ListObjectsV2Command).resolves({})
+      const expectedStatus = 'ok'
+      const expectedBucketStatus = 'up'
+
+      // WHEN
+      const result = await request(app.getHttpServer()).get('/health')
+
+      // THEN
+      expect(result.statusCode).toEqual(HttpStatus.OK)
+      expect(result.body.status).toEqual(expectedStatus)
+      expect(result.body.info.bucket.status).toEqual(expectedBucketStatus)
+    })
+
+    it('returns a 503 SERVICE UNAVAILABLE with bucket status DOWN when bucket is unavailable', async () => {
+      // GIVEN
+      mockS3.on(ListObjectsV2Command).rejects(new Error('Some S3 error'))
+      const expectedStatus = 'error'
+      const expectedBucketStatus = 'down'
+
+      // WHEN
+      const result = await request(app.getHttpServer()).get('/health')
+
+      // THEN
+      expect(result.statusCode).toEqual(HttpStatus.SERVICE_UNAVAILABLE)
+      expect(result.body.status).toEqual(expectedStatus)
+      expect(result.body.error.bucket.status).toEqual(expectedBucketStatus)
+    })
+  })
+})
