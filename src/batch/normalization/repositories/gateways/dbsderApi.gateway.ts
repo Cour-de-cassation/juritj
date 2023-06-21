e6730b16acd8c2afed0de58f0c9d032a1c60871f
Armen Ozcelik <armen.ozcelik@octo.com>
2023-06-21 22:54:24 +0200

    WIP

--- src/batch/normalization/repositories/gateways/dbsderApi.gateway.ts
+++ src/batch/normalization/repositories/gateways/dbsderApi.gateway.ts
@@ -22,7 +22,6 @@ export class DbSderApiGateway {
       )
       .catch((error) => {
         if (error.response) {
-          console.log({ error: error.response })
           if (error.response.data.statusCode === HttpStatus.BAD_REQUEST) {
             logger.error(error.response.data.message)
             throw new BadRequestException(
