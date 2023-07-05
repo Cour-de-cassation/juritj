ffd8c124b164ee6ee0879194fe9a0ec34bfdc7f2
ritchie-octo <129737158+ritchie-octo@users.noreply.github.com>
2023-07-05 11:37:46 +0200

    tech: add env validation with Joi

--- src/.jest/setupEnvVars.ts
+++ src/.jest/setupEnvVars.ts
@@ -1,6 +1,12 @@
 process.env = {
-  S3_BUCKET_NAME_NORMALIZED: 'fake-normalized-bucket-name',
+  DOC_LOGIN: 'login',
+  DOC_PASSWORD: 'password',
+  S3_URL: 's3-url',
+  S3_ACCESS_KEY: 's3-access-key',
+  S3_SECRET_KEY: 's3-secret-key',
+  S3_REGION: 's3-region',
   S3_BUCKET_NAME_RAW: 'fake-raw-bucket-name',
+  S3_BUCKET_NAME_NORMALIZED: 'fake-normalized-bucket-name',
   DBSDER_API_URL: 'http://localhost:3000/v1',
-  DBSDER_API_KEY: 'fakeApiKey'
+  DBSDER_API_KEY: '943a3520-95fe-48fb-82d8-11e55844794d'
 }
---
+++ src/shared/infrastructure/dto/env.validation.ts
@@ -0,0 +1,22 @@
+import * as Joi from 'joi'
+
+export const envValidationConfig = {
+  cache: true,
+  validationSchema: Joi.object({
+    DOC_LOGIN: Joi.string().required(),
+    DOC_PASSWORD: Joi.string().required(),
+    S3_URL: Joi.string().required(),
+    S3_ACCESS_KEY: Joi.string().required(),
+    S3_SECRET_KEY: Joi.string().required(),
+    S3_REGION: Joi.string().required(),
+    S3_BUCKET_NAME_RAW: Joi.string().required(),
+    S3_BUCKET_NAME_NORMALIZED: Joi.string().required(),
+    DBSDER_API_URL: Joi.string().required(),
+    DBSDER_API_KEY: Joi.string()
+      .required()
+      .guid({
+        version: ['uuidv4'],
+        separator: true
+      })
+  })
+}
