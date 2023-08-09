2b38ff51150b8598e98969d13ab59f2fb633a1ab
Fabricio ALEGRIA <f.alegria.herrera@octo.com>
2023-08-09 16:38:21 +0200

    tech(504): add PR feedbacks

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,15 +1,15 @@
-import { Module } from '@nestjs/common'
-import { HttpModule } from '@nestjs/axios'
 import { ConfigModule } from '@nestjs/config'
+import { HttpModule } from '@nestjs/axios'
+import { LoggerModule } from 'nestjs-pino'
+import { Module } from '@nestjs/common'
 import { TerminusModule } from '@nestjs/terminus'
 import { RedirectController } from './app.controller'
 import { Context } from '../shared/infrastructure/utils/context'
+import { pinoConfig } from '../shared/infrastructure/utils/pinoConfig.utils'
 import { HealthController } from './infrastructure/controllers/health/health.controller'
 import { DecisionsController } from './infrastructure/controllers/decisions/decisions.controller'
 import { BucketHealthIndicator } from './infrastructure/controllers/health/bucketHealthIndicator'
 import { envValidationConfig } from '../shared/infrastructure/dto/env.validation'
-import { pinoConfig } from '../shared/infrastructure/utils/pinoConfig.utils'
-import { LoggerModule } from 'nestjs-pino'
 
 @Module({
   imports: [
