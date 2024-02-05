d71a1774b5ac452b4b2641ddce2a38a8127b6832
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2024-02-05 17:33:36 +0100

    linting

--- src/api/infrastructure/exceptions/missingProperties.exception.ts
+++ src/api/infrastructure/exceptions/missingProperties.exception.ts
@@ -3,16 +3,14 @@ import { HttpException, HttpStatus } from '@nestjs/common'
 export class BadPropertiesException extends HttpException {
   constructor(missingProperties: string[], value: Record<string, any>) {
     super(
-      JSON.stringify(
-        {
-          missingProperties: missingProperties,
-          codeJuridiction: value.codeJuridiction,
-          codeService: value.codeService,
-          dateDecision: value.dateDecision,
-          numeroRoleGeneral: value.numeroRoleGeneral,
-          numeroRegistre: value.numeroRegistre
-        }
-      ),
+      JSON.stringify({
+        missingProperties: missingProperties,
+        codeJuridiction: value.codeJuridiction,
+        codeService: value.codeService,
+        dateDecision: value.dateDecision,
+        numeroRoleGeneral: value.numeroRoleGeneral,
+        numeroRegistre: value.numeroRegistre
+      }),
       HttpStatus.BAD_REQUEST
     )
   }
