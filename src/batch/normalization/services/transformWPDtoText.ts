7165fa1d62625544858f3ecfe0efadca8bda32b8
Antoine Jeanneney <29945628+ajeanneney@users.noreply.github.com>
2024-06-13 15:47:47 +0200

    Fix/file name (#288)
    * fix filename issue
    
    * escape quotes in filenames
    
    ---------
    
    Co-authored-by: Antoine Jeanneney <antoine.jeanneney@justice.fr>

--- src/batch/normalization/services/transformWPDtoText.ts
+++ src/batch/normalization/services/transformWPDtoText.ts
@@ -1,4 +1,4 @@
-import { existsSync } from 'fs'
+import { existsSync, statSync } from 'fs'
 import { promisify } from 'util'
 import { exec } from 'child_process'
 import { logger, normalizationFormatLogs } from '../index'
@@ -15,7 +15,16 @@ export async function readWordperfectDocument(filename: string) {
   const cmdPath = await getConversionCommandPath(CONVERSION_COMMAND)
   if (cmdPath && existsSync(filename)) {
     try {
-      const { stdout } = await execPromise("wpd2text '" + filename + "'")
+      if (!statSync(filename).isFile()) {
+        logger.error({
+          ...normalizationFormatLogs,
+          msg: `Path provided is not a file: ${filename}`
+        })
+        throw new Error()
+      }
+
+      // Escape the quote character in the filename in case of renormalization of decisions received at the beginning of the project that may contain quotes in filenames.
+      const { stdout } = await execPromise("wpd2text '" + filename.replace(/'/g, `'\\''`) + "'")
       return stdout
     } catch (error) {
       logger.error({
