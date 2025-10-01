99a14be089217b98346b04b0b241e7e2f6d435b0
Julien Grach <julien.grach@justice.fr>
2025-10-01 12:14:10 +0200

    remove batch from ci/cd

--- src/.jest/setupEnvVars.ts
+++ src/.jest/setupEnvVars.ts
@@ -6,8 +6,6 @@ process.env = {
   S3_SECRET_KEY: 's3-secret-key',
   S3_REGION: 's3-region',
   S3_BUCKET_NAME_RAW: 'fake-raw-bucket-name',
-  S3_BUCKET_NAME_NORMALIZED: 'fake-normalized-bucket-name',
   DBSDER_API_URL: 'http://localhost:3000',
-  DBSDER_API_KEY: '9ad44ab5-3416-4989-90a6-3cfe972175de',
-  COMMISSIONING_DATE: '2023-12-15'
+  DBSDER_API_KEY: '9ad44ab5-3416-4989-90a6-3cfe972175de'
 }
--- src/shared/infrastructure/dto/env.validation.ts
+++ src/shared/infrastructure/dto/env.validation.ts
@@ -10,14 +10,12 @@ export const envValidationConfig = {
     S3_SECRET_KEY: Joi.string().required(),
     S3_REGION: Joi.string().required(),
     S3_BUCKET_NAME_RAW: Joi.string().required(),
-    S3_BUCKET_NAME_NORMALIZED: Joi.string().required(),
     DBSDER_API_URL: Joi.string().required(),
     DBSDER_API_KEY: Joi.string()
       .required()
       .guid({
         version: ['uuidv4'],
         separator: true
-      }),
-    COMMISSIONING_DATE: Joi.string()
+      })
   })
 }
